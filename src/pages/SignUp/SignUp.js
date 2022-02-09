763ca717750d8b44e36a2285b95517857f739b0b
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-02-09 12:44:00 -0300

    Add basic functionality to sign up page

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,77 @@
+import { useState } from "react";
+
+import api from "../../services/api";
+import { Button, Input } from "../../styledComponents";
+
+import { Form } from "./style";
+
+// TODO: disable button while loading
+// TODO: in case of success, navigate to "/log-in"
+
 export default function SignUp() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  function handleChange({ target }) {
+    setFormData({ ...formData, [target.name]: target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (formData.password !== formData.repeatPassword) {
+      alert("Você inseriu senhas diferentes");
+      return;
+    }
+
+    const newUser = { ...formData };
+    delete newUser.repeatPassword;
+
+    try {
+      await api.signUp(newUser);
+    } catch (error) {
+      alert(error.response.data);
+    }
+  }
+
   return (
-    <section>
-      <h2>SignUp</h2>
-    </section>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        placeholder="Nome"
+        name="name"
+        onChange={handleChange}
+        value={formData.name}
+      />
+      <Input
+        type="email"
+        placeholder="E-mail"
+        name="email"
+        onChange={handleChange}
+        value={formData.email}
+      />
+      <Input
+        type="password"
+        placeholder="Senha"
+        name="password"
+        onChange={handleChange}
+        value={formData.password}
+      />
+      <Input
+        type="password"
+        placeholder="Confirme a senha"
+        name="repeatPassword"
+        onChange={handleChange}
+        value={formData.repeatPassword}
+      />
+
+      <Button type="submit" highlighted>
+        Cadastrar-se
+      </Button>
+    </Form>
   );
 }
