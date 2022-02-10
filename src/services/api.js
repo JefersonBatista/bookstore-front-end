c7b2ffc51968b6e0bf360e1959d39be63ee20ea7
Leonardo Otero <leonardo.otero390@gmail.com>
2022-02-10 09:35:33 -0400

    chore: deploy env vars

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "https://bookstore390.herokuapp.com";
-
+const BASE_URL =
+  process.env.REACT_APP_API_URL === "dev"
+    ? "http://localhost:4000"
+    : "https://bookstore390.herokuapp.com";
+    
 function createAuth(token) {
   return { headers: { Authorization: `Bearer ${token}` } };
 }
