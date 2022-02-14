0f8e8c7515dec1b2326ad1e2dce9691dd8e3790b
Leonardo Otero <leonardo.otero390@gmail.com>
2022-02-14 13:58:10 -0400

    fix: clean localstorage even if no conection

--- src/components/TopBar/LogoutButton.js
+++ src/components/TopBar/LogoutButton.js
@@ -1,20 +1,18 @@
 import { FiLogOut } from "react-icons/fi";
-import requests from "../services/api";
+import requests from "../../services/api";
 
 export default function LogoutButton({ token, navigate }) {
   function requestLogout() {
     requests
       .logout(token)
-      .then(() => {
-        localStorage.removeItem("session");
-        navigate("/");
-      })
-      .catch((err) => {
-        console.error(err);
+      .then()
+      .catch(() => {
         alert(
-          "Não foi possível desconectar, verifique sua conexão ou reset os dados da página"
+          "Não foi possível desconectar, mas os dados foram apagados do navegador,verifique a conexão e tente logar novamente para deslogar e proteger sua conta"
         );
       });
+    navigate("/");
+    localStorage.removeItem("session");
   }
   return (
     <button style={{ all: "unset" }} onClick={requestLogout}>
